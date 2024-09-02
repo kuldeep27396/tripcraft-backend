9a0680451e3084aba638460a2fa31f93581a3cd3
kuldeep27396 <kuldeep27396@gmail.com>
2024-09-02 17:46:40 +0530

    added api code

--- src/main/java/com/tripcraft/backend/TripcraftBackendApplication.java
+++ src/main/java/com/tripcraft/backend/TripcraftBackendApplication.java
@@ -1,14 +1,20 @@
 package com.tripcraft.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 public class TripcraftBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST", "localhost"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT", "5432"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME", "tripcraft"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME", "default_username"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD", "default_password"));
+
 		SpringApplication.run(TripcraftBackendApplication.class, args);
 	}
 }
